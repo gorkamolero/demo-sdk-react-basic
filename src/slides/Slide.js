83379948f049fab75c9cc5e1f961989cb1b03472
Gorka <gorka.molero@gmail.com>
2020-11-17 14:12:14 +0100

    Title mobile

--- src/slides/Slide.js
+++ src/slides/Slide.js
@@ -92,7 +92,7 @@ const Slide = () => {
       <FlexBox column center className={`slide-${type} slide-${slideId && slideId} animate`}>
         <div className={`HbHeadContainer ${(!slideTitle === 'Profile' || isEndSlide) ? 'hidingImage' : 'showingImage'} ${isEndSlide ? 'isEndSlide' : 'isNotEndSlide'} ${nextSlideIsEndSlide ? 'nextSlideIsEndSlide' : ''}`} ref={HeadRef}>
           <HbHeader
-            className={`HbHeader ${(!slideTitle === 'Profile' || isEndSlide) ? 'hideImage' : ''} ${loading && isEndSlide ? 'isLoading' : 'finishedLoading'} ${!title && !subtitle ? 'noTitleNoSubtitle' : ''} ${title && !subtitle ? 'titleNoSubtitle' : ''} ${!title && subtitle ? 'noTitleSubtitle' : ''}`}
+            className={`HbHeader ${(!slideTitle === 'Profile' || isEndSlide) ? 'hideImage' : ''} ${loading && isEndSlide ? 'isLoading' : 'finishedLoading'} ${!title && !subtitle ? 'noTitleNoSubtitle' : ''} ${title && !subtitle ? 'titleNoSubtitle' : ''} ${!title && subtitle ? 'noTitleSubtitle' : ''} ${isFirstSlide ? 'isFirstSlide' : ''} `}
             TitleSlot={<HbTitle data-size={size} size={size} className="title" html={title} />}
             SubtitleSlot={<HbSubtitle data-size={size} size={size} className="subtitle" html={subtitle} />}
             HbLogo={<HbHeader.HbLogo className="HbLogo" onClick={event =>  window.location.href='/'} />}
