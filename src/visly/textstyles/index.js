b9dccdf85545cabbb37e17ff4ead312f8ff37253
Gorka <gorka.molero@gmail.com>
2020-12-01 19:36:37 +0100

    Min height for products

--- src/visly/textstyles/index.js
+++ src/visly/textstyles/index.js
@@ -47,7 +47,7 @@ export const bodySmallMobile = {
 };
 export const hbFeatureText = {
   fontFamily: "Averta",
-  fontSize: "17px",
+  fontSize: "16px",
   fontWeight: 400,
   fontStyle: "normal",
   lineHeight: "21px",
