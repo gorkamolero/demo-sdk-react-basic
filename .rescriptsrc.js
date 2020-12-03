06f84b53de74b197d4ab4651f2768e52116c8cae
Isidro García Hernández <isidrogarciahernandez@gmail.com>
2020-12-03 14:22:31 +0100

    fix rescripts

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,25 +1,38 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-let version = process.argv[2];
+if (process.argv[1].indexOf('build.js')!=-1) {
+    // Build
+    let version = process.argv[2];
 
-module.exports = config => {
-    config.output.filename = `hb-${version}.js`;
-    config.output.chunkFilename = `hb-${version}.js`;
+    module.exports = config => {
+        config.output.filename = `hb-${version}.js`;
+        config.output.chunkFilename = `hb-${version}.js`;
 
-    config.plugins = [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: `hb-${version}.css`,
-            chunkFilename: `hb-${version}.css`,
-        })
-    ]
+        config.plugins = [
+            new MiniCssExtractPlugin({
+                filename: `hb-${version}.css`,
+                chunkFilename: `hb-${version}.css`,
+            })
+        ]
 
-    config.optimization.runtimeChunk = false;
-    config.optimization.splitChunks = {
-        cacheGroups: {
-            default: false
-        }
+        config.optimization.runtimeChunk = false;
+        config.optimization.splitChunks = {
+            cacheGroups: {
+                default: false
+            }
+        };
+        return config;
     };
-    return config;
-};
+} else {
+    // Start
+    module.exports = config => {
+        config.optimization.runtimeChunk = false;
+        config.optimization.splitChunks = {
+            cacheGroups: {
+                default: false
+            }
+        };
+        return config;
+    };
+}
+
