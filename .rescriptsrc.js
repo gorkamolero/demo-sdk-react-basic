a1e1f2c41969da1a06b694f250cf86cdece11b91
Gorka <gorka.molero@gmail.com>
2020-12-03 14:20:14 +0100

    Commenting config plugins on rescripts file

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -6,14 +6,14 @@ module.exports = config => {
     config.output.filename = `hb-${version}.js`;
     config.output.chunkFilename = `hb-${version}.js`;
 
-    config.plugins = [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: `hb-${version}.css`,
-            chunkFilename: `hb-${version}.css`,
-        })
-    ]
+    // config.plugins = [
+    //     new MiniCssExtractPlugin({
+    //         // Options similar to the same options in webpackOptions.output
+    //         // both options are optional
+    //         filename: `hb-${version}.css`,
+    //         chunkFilename: `hb-${version}.css`,
+    //     })
+    // ]
 
     config.optimization.runtimeChunk = false;
     config.optimization.splitChunks = {
